98825b3e2f36548040dac5e99efc5049584ff900
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-03 20:26:20 -0400

    Higher-Order Ex 1 & 2

--- Eloquent/index.js
+++ Eloquent/index.js
@@ -22,14 +22,47 @@ const SCRIPTS = require('./script.js');
 //     }
 //     console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));
 //     // → 10
-function characterCount(script) {
-    return script.ranges.reduce((count, [from, to]) => {
-    return count + (to - from); //reduces the amount of characters in one script
-    }, 0);
-    }
-    console.log(SCRIPTS.reduce((a, b) => { //code begins here
-        //the array is fed with the . already, so you just need the combine
-    return characterCount(a) < characterCount(b) ? b : a; //reducec the count of two scripts and returns the largest
-    }));
-    // → {name: "Han", ...}
+// function characterCount(script) {
+//     return script.ranges.reduce((count, [from, to]) => {
+//     return count + (to - from); //reduces the amount of characters in one script
+//     }, 0);
+//     }
+//     console.log(SCRIPTS.reduce((a, b) => { //code begins here
+//         //the array is fed with the . already, so you just need the combine
+//     return characterCount(a) < characterCount(b) ? b : a; //reducec the count of two scripts and returns the largest
+//     }));
+//     // → {name: "Han", ...}
     
+// function characterScript(code) {
+//     for (let script of SCRIPTS) {
+//     if (script.ranges.some(([from, to]) => {
+//     return code >= from && code < to;
+//     })) {
+//     return script;
+//     }
+//     }
+//     return null;
+//     }
+    
+
+//     console.log(characterScript(121));
+//     // → {name: "Latin", ...}
+    
+// const arrOfArr = [[1,2],[2,3],[3,3,4,2]];
+// // =======1-Flattening========
+// let flatten = (arr) => {
+//     let newArr= [];
+//     newArr = arr.reduce((count, deepArr) => count.concat(deepArr));
+//     return newArr;
+// }
+// console.log(flatten(arrOfArr));
+
+// =======2-YourOwnLoop========
+
+// let myLoop = (value, testFunc, updateFunc, bodyFunction) => {
+//     for (let start = value; testFunc(start); start = updateFunc(start)) {bodyFunction(start)};
+// }
+// myLoop(3, n => n > 0, n => --n, n => console.log(n));
+
+// =======3-Everything========
+let every1 = ()
