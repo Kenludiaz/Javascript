dd405b2b48455652199dde66df54f7f3fda07596
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-16 21:20:56 -0400

    Moving on to excercise 3.

--- Eloquent/SLO.js
+++ Eloquent/SLO.js
@@ -29,14 +29,34 @@
 
 // Ex. 2
 
-class Set {
-    constructor(...args) {
-        this.items = [...args];
-    };
-    add = (...args) => {
-        this.items = [this.items, ...args];
-    }
-    delete = (arg) => {
-        if ()
-    }
-}
+// class Group {
+//     constructor() {
+//       this.members = [];
+//     }
+  
+//     add(value) {
+//       if (!this.has(value)) {
+//         this.members.push(value);
+//       }
+//     }
+  
+//     delete(value) {
+//       this.members = this.members.filter(v => v !== value);
+//     }
+  
+//     has(value) {
+//       return this.members.includes(value);
+//     }
+  
+//     static from(collection) {
+//       let group = new Group;
+//       for (let value of collection) {
+//         group.add(value);
+//       }
+//       return group;
+//     }
+//   }
+  
+
+// Ex. 3
+
