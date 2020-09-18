424dd4ae064d26725e0f920fc559e3af1c10a1f4
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-17 21:18:17 -0400

    .call method takes in two arguments: what to use for this and the argument (one) for the method it's chained to (hasOwnProperty).

--- Eloquent/SLO.js
+++ Eloquent/SLO.js
@@ -60,52 +60,60 @@
 
 // Ex. 3
 
-class Group {
-    constructor() {
-      this.members = [];
-    }
+// class Group {
+//     constructor() {
+//       this.members = [];
+//     }
   
-    add(value) {
-      if (!this.has(value)) {
-        this.members.push(value);
-      }
-    }
+//     add(value) {
+//       if (!this.has(value)) {
+//         this.members.push(value);
+//       }
+//     }
   
-    delete(value) {
-      this.members = this.members.filter(v => v !== value);
-    }
+//     delete(value) {
+//       this.members = this.members.filter(v => v !== value);
+//     }
   
-    has(value) {
-      return this.members.includes(value);
-    }
+//     has(value) {
+//       return this.members.includes(value);
+//     }
   
-    static from(collection) {
-      let group = new Group;
-      for (let value of collection) {
-        group.add(value);
-      }
-      return group;
-    }
+//     static from(collection) {
+//       let group = new Group;
+//       for (let value of collection) {
+//         group.add(value);
+//       }
+//       return group;
+//     }
   
-    [Symbol.iterator]() {
-      return new GroupIterator(this);
-    }
-  }
+//     [Symbol.iterator]() {
+//       return new GroupIterator(this);
+//     }
+//   }
   
-  class GroupIterator {
-    constructor(group) {
-      this.group = group;
-      this.position = 0;
-    }
+//   class GroupIterator {
+//     constructor(group) {
+//       this.group = group;
+//       this.position = 0;
+//     }
   
-    next() {
-      if (this.position >= this.group.members.length) {
-        return {done: true};
-      } else {
-        let result = {value: this.group.members[this.position],
-                      done: false};
-        this.position++;
-        return result;
-      }
-    }
-  }
+//     next() {
+//       if (this.position >= this.group.members.length) {
+//         return {done: true};
+//       } else {
+//         let result = {value: this.group.members[this.position],
+//                       done: false};
+//         this.position++;
+//         return result;
+//       }
+//     }
+//   }
+
+
+// Ex. 4
+
+let map = {one: true, two: true, hasOwnProperty: true};
+
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
+// → true
