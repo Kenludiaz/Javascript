25bddf830e2135a26b5e69e6dffe5fb4be2129a5
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-28 10:04:42 -0400

    Saved to local database file.

--- Data/2node/index.js
+++ Data/2node/index.js
@@ -1,15 +1,28 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+const Datastore = require('nedb');
+
 
 app.listen(3000, () => console.log("Listening at 3000"));
 app.use(express.static("public"));
 app.use(express.json());
 
-let collection = [];
+const database = new Datastore('database.db')
+database.loadDatabase();
+
+
 app.post('/api', (req,res) => {
     const data = req.body;
-    collection.push(data);
-    console.log(req);
-    res.json({collection})
+
+    let timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+
+    res.json({
+        status: 'success',
+        timestamp: timestamp,
+        latitude: data.lat,
+        longitude: data.lon
+    })
 })
