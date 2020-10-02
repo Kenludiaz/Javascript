3f3337f10031007b989b89ac32ee2aee90910039
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-10-02 13:48:37 -0400

    Making calls from the server.

--- Data/3node/index.js
+++ Data/3node/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const Datastore = require('nedb')
 const app = express();
@@ -11,15 +12,22 @@ app.listen(port, () => {
 
 app.get('/api/:latlon', async (req, res) => {
     let latlon = req.params.latlon;
-    console.log(latlon);
+    // console.log(latlon);
     latlon.split(",");
-    console.log(latlon);
+    // console.log(latlon);
+    let lat = latlon[0];
+    let lon = latlon[1];
 
+    const API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&id=524901&APPID=e3e7261bccb6e158fae3081c9fed0167`
+    
+    let weather = await fetch(API_URL);
+    let format = await weather.json();
+    console.log(format);
+    response.json(format);
+
+
+
+    app.use(express.json());
+    app.use(express.static('public'));
 
 })
-
-
-
-app.use(express.json());
-app.use(express.static('public'));
-
